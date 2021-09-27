f4f9608bff2288625b2e2d89a13f4b042092ab47
Luke Wainhouse <lukewainhouse@hotmail.com>
2021-09-27 20:53:59 +0100

    Avatar

--- instagram-clone-react/src/Post.js
+++ instagram-clone-react/src/Post.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import './Post.css';
-
+import Avatar from '@mui/material/Avatar';
 
 function Post() {
     return (
         <div className="post">
             <div className="post">
-                {/* <Avatar
+                <Avatar
                     className="post__avatar"
-                    alt="Wainhouse" src="/static/images/avatar/1.jpg"
-                /> */}
+                    alt="Wainhouse" src="/static/images/avatar/1.jpg" />
                 <h3>Username</h3>
             </div>
 
             {/* /header -> avatar + username/ */}
 
-            <img className="post__image" src="https://www.freecodecamp.org/news/content/images/size/w600/2021/06/Ekran-Resmi-2019-11-18-18.08.13.png" />
+            <img className="post__image" alt=" " src="https://www.freecodecamp.org/news/content/images/size/w600/2021/06/Ekran-Resmi-2019-11-18-18.08.13.png" />
             {/* /image/ */}
 
 
