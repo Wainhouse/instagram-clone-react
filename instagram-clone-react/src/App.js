11c7ff2094b93de37d53eeec734de9970497f5cd
Luke Wainhouse <lukewainhouse@hotmail.com>
2021-09-27 16:28:59 +0100

    changed App.css

--- instagram-clone-react/src/App.js
+++ instagram-clone-react/src/App.js
@@ -4,7 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="app">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
+          alt=""
+        />
+      </div>
       <h1> Everybody is Welcome</h1>
+
+      {/* /Post/ */}
+      {/* /Post/ */}
     </div>
   );
 }
