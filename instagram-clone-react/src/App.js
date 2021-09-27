291bbd82517476bb70774136dfd88c2147f41a95
Luke Wainhouse <lukewainhouse@hotmail.com>
2021-09-27 23:47:38 +0100

    sign up button

--- instagram-clone-react/src/App.js
+++ instagram-clone-react/src/App.js
@@ -6,15 +6,17 @@ import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { positions } from '@mui/system';
+import { Input } from '@mui/material';
 
 const style = {
-  position: 'absolute',
-  top: '50',
-  left: '50',
-  transform: 'translate(-50%, -50%)',
+  position: 'aboslute',
+  top: '100%',
+  left: '100%',
+  transform: 'translate(+160%, +170%)',
   width: 400,
   bgcolor: 'background.paper',
-  border: '2px solid #000',
+  border: '1px solid lightgray',
   boxShadow: 24,
   p: 4,
 };
@@ -22,8 +24,11 @@ const style = {
 function App() {
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
-  // useEffect -> Runs a piece of code based on a specific condition
+  const [username, setUsername] = useState([]);
+  const [email, setEmail] = useState([]);
+  const [password, setPassword] = useState([]);
 
+  // useEffect -> Runs a piece of code based on a specific condition
   useEffect(() => {
     //this is where the code runs
     db.collection('posts').onSnapshot(snapshot => {
@@ -46,8 +51,38 @@ function App() {
         onClose={() => setOpen(false)}
       >
         <Box sx={style}>
+          <div>
+            <center>
+              <img
+                className="app__header-image"
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
+                alt=""
+              />
+              <Input
+                placeholder="text"
+                type="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <Input
+                placeholder="email"
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input
+                placeholder="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+
+              <Button onClick={signUp}>Sign up</Button>
+
+            </center>
+          </div>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-            I am a Modal
+
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
             Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
@@ -61,9 +96,7 @@ function App() {
           alt=""
         />
       </div>
-
-      <Button onClick={ }>Sign Up</Button>
-
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
       <h1>Everybody is Welcome</h1>
 
       {
