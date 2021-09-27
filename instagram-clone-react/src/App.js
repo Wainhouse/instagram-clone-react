4e6f6428f16d495578c5911709266537a88a56d4
Luke Wainhouse <lukewainhouse@hotmail.com>
2021-09-27 23:20:07 +0100

    sign up button

--- instagram-clone-react/src/App.js
+++ instagram-clone-react/src/App.js
@@ -2,10 +2,26 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './Post.js';
 import { db } from './firebase';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const style = {
+  position: 'absolute',
+  top: '50',
+  left: '50',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 function App() {
   const [posts, setPosts] = useState([]);
-
+  const [open, setOpen] = useState(false);
   // useEffect -> Runs a piece of code based on a specific condition
 
   useEffect(() => {
@@ -19,8 +35,25 @@ function App() {
     })
   }, []);
 
+  const signUp = (event) => {
+
+  }
+
   return (
     <div className="app">
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            I am a Modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
       <div className="app__header">
         <img
           className="app__header-image"
@@ -28,6 +61,8 @@ function App() {
           alt=""
         />
       </div>
+
+      <Button onClick={ }>Sign Up</Button>
 
       <h1>Everybody is Welcome</h1>
 
